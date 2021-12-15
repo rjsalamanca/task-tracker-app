70021cabaaf5eacd29b0340cc0605886e297ee53
rjsalamanca <salamancaronito@gmail.com>
2021-12-15 18:38:27 -0500

    working with hook

--- src/App.js
+++ src/App.js
@@ -6,22 +6,38 @@ function App() {
    const [tasks, setTasks] = useState([
       {
          id: 1,
-         name: 'test 1'
+         text: 'Doctors Appointment',
+         day: 'Feb 5th at 2:30pm',
+         reminder: true
       },
       {
          id: 2,
-         name: 'test 2'
+         text: 'Meeting at School',
+         day: 'Feb 5th at 3:30pm',
+         reminder: true
       },
       {
          id: 3,
-         name: 'test 3'
+         text: 'Food Shopping',
+         day: 'Feb 5th at 4:30pm',
+         reminder: false
       },
    ]);
+
+   // Delete Task
+   const deleteTask = (id) => {
+      setTasks(tasks.filter(task => task.id !== id));
+   }
+
+   // Toggle Reminder
+   const toggleReminder = (id) => {
+      setTasks(tasks.map(task => task.id === id ? { ...task, reminder: !task.reminder } : task))
+   }
 
    return (
       <div className="container">
          <Header />
-         <Tasks tasks={tasks} />
+         {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks to Show'}
       </div>
    );
 }
